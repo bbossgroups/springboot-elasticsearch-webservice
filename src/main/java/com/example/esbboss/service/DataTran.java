6f8ac623ea2eb3decd5f5bd3127255b42f6b64c7
yinbp <yin-bp@163.com>
2020-04-25 20:42:16 +0800

    # v6.1.0 功能改进 1. 数据同步工具改进
      如果在程序里面配置的增量字段类型发生改变，要把增量状态表对应的类型调整为最新的字段类型
    
      设置了类型后，就按照类型来，不再按照设置的日期和数字字段名称来区分：
       
      增加setLastValueColumn方法，废弃setDateLastValueColumn和setNumberLastValueColumn两个方法
    2. DB-DB数据同步工具：增加在作业中直接指定sql语句的功能
    3. 修复数据同步bug：数据库到数据库跨库同步无效

--- src/main/java/com/example/esbboss/service/DataTran.java
+++ src/main/java/com/example/esbboss/service/DataTran.java
@@ -123,7 +123,7 @@ public class DataTran {
 					//指定导入数据的sql语句，必填项，可以设置自己的提取逻辑，
 					// 设置增量变量log_id，增量变量名称#[log_id]可以多次出现在sql语句的不同位置中，例如：
 					// select * from td_sm_log where log_id > #[log_id] and parent_id = #[log_id]
-					// log_id和数据库对应的字段一致,就不需要设置setNumberLastValueColumn和setNumberLastValueColumn信息，
+					// log_id和数据库对应的字段一致,就不需要设置setLastValueColumn信息，
 					// 但是需要设置setLastValueType告诉工具增量字段的类型
 
 					importBuilder.setSql("select * from td_sm_log where LOG_OPERTIME > #[LOG_OPERTIME]");
@@ -183,8 +183,7 @@ public class DataTran {
 //		});
 //		//设置任务执行拦截器结束，可以添加多个
 					//增量配置开始
-//		importBuilder.setNumberLastValueColumn("log_id");//手动指定数字增量查询字段，默认采用上面设置的sql语句中的增量变量名称作为增量查询字段的名称，指定以后就用指定的字段
-//		importBuilder.setDateLastValueColumn("log_id");//手动指定日期增量查询字段，默认采用上面设置的sql语句中的增量变量名称作为增量查询字段的名称，指定以后就用指定的字段
+//		importBuilder.setLastValueColumn("log_id");//手动指定数字增量查询字段，默认采用上面设置的sql语句中的增量变量名称作为增量查询字段的名称，指定以后就用指定的字段
 					importBuilder.setFromFirst(true);//任务重启时，重新开始采集数据，true 重新开始，false不重新开始，适合于每次全量导入数据的情况，如果是全量导入，可以先删除原来的索引数据
 					importBuilder.setLastValueStorePath("logtable_import");//记录上次采集的增量字段值的文件路径，作为下次增量（或者重启后）采集数据的起点，不同的任务这个路径要不一样
 //		importBuilder.setLastValueStoreTableName("logs");//记录上次采集的增量字段值的表，可以不指定，采用默认表名increament_tab
@@ -423,8 +422,7 @@ public class DataTran {
 					//hbase表中列名，由"列族:列名"组成
 //		//设置任务执行拦截器结束，可以添加多个
 //		//增量配置开始
-////		importBuilder.setNumberLastValueColumn("Info:id");//指定数字增量查询字段变量名称
-//		importBuilder.setDateLastValueColumn("Info:logOpertime");//手动指定日期增量查询字段变量名称
+////		importBuilder.setLastValueColumn("Info:id");//指定数字增量查询字段变量名称
 					importBuilder.setFromFirst(true);//任务重启时，重新开始采集数据，true 重新开始，false不重新开始，适合于每次全量导入数据的情况，如果是全量导入，可以先删除原来的索引数据
 					importBuilder.setLastValueStorePath("hbase233esdemo_import");//记录上次采集的增量字段值的文件路径，作为下次增量（或者重启后）采集数据的起点，不同的任务这个路径要不一样
 					//指定增量字段类型为日期类型，如果没有指定增量字段名称,则按照hbase记录时间戳进行timerange增量检索
